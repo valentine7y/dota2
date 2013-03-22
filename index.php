80e829c8ffb6688042482c9af5f4de4cd72008ea
marcaum54 <marcaum54@gmail.com>
2013-03-22 17:50:20 -0300

    update gitignore

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 require_once 'Dota2_API.php';
 
-$api = new Dota2_API('<YOUR_STEAM_API_KEY>');
+$api = new Dota2_API('72F82C4B0CBCC1704CDD2130E685E767');
 
 //Dota2_API::dump($api->getHeroes(), false);
 //Dota2_API::dump($api->getItems(), false);
 //Dota2_API::dump($api->downloadHeroesImages(), false);
-//Dota2_API::dump($api->downloadItemsImages(), false);
+Dota2_API::dump($api->downloadItemsImages(), false);
